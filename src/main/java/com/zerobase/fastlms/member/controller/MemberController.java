ce1f9e83d11a0ed034006a0aa8952f9c34f5d477
Juwon <tjwndnjs1998@gmail.com>
2022-08-18 05:27:57 +0900

    회원가입 및 인증메일 구현

--- src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -1,15 +1,21 @@
 package com.zerobase.fastlms.member.controller;
 
 import com.zerobase.fastlms.member.model.MemberInput;
+import com.zerobase.fastlms.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/member/register")
     public String register() {
@@ -18,11 +24,28 @@ public class MemberController {
     }
 
     @PostMapping("/member/register")
-    public String registerSubmit(HttpServletRequest request, HttpServletResponse response
+    public String registerSubmit(Model model, HttpServletRequest request, HttpServletResponse response
             , MemberInput parameter) {
 
-        System.out.println(parameter.toString());
+        boolean result = memberService.register(parameter);
+        model.addAttribute("result", result);
 
-        return "member/register";
+
+        return "member/register_complete";
     }
+
+    @GetMapping("/member/email-auth")
+    public String emailAuth(Model model, HttpServletRequest request){
+
+        String uuid = request.getParameter("id");
+
+        System.out.println(uuid);
+
+        boolean result = memberService.emailAuth(uuid);
+        model.addAttribute("result", result);
+
+        return "member/email_auth";
+    }
+
+
 }
